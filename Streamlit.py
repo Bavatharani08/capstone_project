be8b90b1449665e9ab000cf407c558402c351b5c
Bavatharani <82824516+Bavatharani08@users.noreply.github.com>
2023-07-06 17:33:08 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -11,9 +11,32 @@
 
 
 import streamlit as st
-st.write("Hello")
+
+def user_input_features():
+        review=st.text_input("statement", default_value_goes_here)
 
 
+        data = {
+                'review': review,
+                
+                }
+        features = pd.DataFrame(data, index=[0])
+        return features
+
+#App Title
+st.title("Sentiment predictor")
+
+#st.write("Hello")
+
+st.write("""
+### Mobile Price Prediction App
+This app predicts the **Mobile Price Range**
+""")
+st.sidebar.header('User Input Features')
+st.subheader('User Input parameters')
+df_features = user_input_features()
+
+st.write(df_features)
 # In[ ]:
 
 
