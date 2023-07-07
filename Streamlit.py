e62de207121cb28623b6e4b4da2a9a561dd760d7
Bavatharani <82824516+Bavatharani08@users.noreply.github.com>
2023-07-07 17:02:30 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -16,7 +16,7 @@ from PIL import Image
 
 
 #App Title
-st.title("Sentiment Analyzer")
+st.title("SENTIMENT ANALYZER")
 image = Image.open('sentiment_analyzer_background.png')
 new_image = image.resize((1000, 400))
 st.image(new_image)
@@ -24,13 +24,25 @@ st.image(new_image)
 #st.write("Hello")
 
 st.write("""
-### SENTIMENT ANALYZER
 This app detects the **EMOTIONS** in the data
 """)
 #st.sidebar.header('User Input Features')
 st.title('Text analyser')
 TextBox = st.text_area('Enter text here...')
 
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #0099ff;
+    color:#ffffff;
+}
+div.stButton > button:hover {
+    background-color: #00ff00;
+    color:#ff0000;
+    }
+</style>""", unsafe_allow_html=True)
+
+b = st.button("Analyse")
 if st.button('Analyse'):
     st.write(TextBox)
 
